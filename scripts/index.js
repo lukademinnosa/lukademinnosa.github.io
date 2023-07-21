9d215dbe05e42ca2d40bc4ac70bebd4cdc164ff9
Ð⨊⩔ ⨙ɤαηΚ <lucasfrank254@gmail.com>
2023-07-21 12:57:51 +0300

    Done with project

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,37 @@
-/**
- * This code demonstrates how to implement iterators and generators in JavaScript.
- * It loads code from a json file and then iterates over it using a generator.
- * It then displays the results in the index page.
- */
-
 // Create a generator to iterate over the data
-
 function* CreateIterator(data) {
   for (let i = 0; i < data.length; i += 1) {
     yield data[i];
   }
 }
 
+async function displayQuote(iterator) {
+  const result = iterator.next();
+
+  if (!result.done) {
+    const quote = result.value;
+    const quoteElement = document.getElementById('quote');
+    quoteElement.innerHTML = quote.quote;
+    document.getElementById('author').innerHTML = quote.author;
+    setTimeout(displayQuote, 5000); // Delay of 5 seconds
+  }
+}
 // Load the data from the json file
+async function FetchData() {
+  try {
+    const response = await fetch('./scripts/quotes.json');
+    const data = await response.json();
 
-function FetchData() {
-  fetch('./scripts/quotes.json')
-    .then((response) => response.json())
-    .then((data) => {
-      // Create a generator to iterate over the data
-      const iterator = CreateIterator(data.quotes);
+    // Create a generator to iterate over the data
+    const iterator = CreateIterator(data.quotes);
 
-      // Iterate over the data
-      let result = iterator.next();
-      while (!result.done) {
-        // Display the data
-        const quote = result.value;
-        const quoteElement = document.createElement('div');
-        quoteElement.className = 'quote';
-        quoteElement.innerHTML = quote.quote;
-        document.body.appendChild(quoteElement);
+    // Start displaying quotes
+    displayQuote(iterator);
+  } catch (error) {
 
-        // Move to the next item
-        result = iterator.next();
-      }
-    });
+    // console.error("Error fetching data:", error);
+  }
 }
 
-// Call the function to load the data
+// Call the function to load the data and start displaying quotes
 FetchData();
